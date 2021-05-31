47ce6481264c910719bf5d0a94885acb0bcc2e99
Colorinche <68034479+AndreaLecuona@users.noreply.github.com>
2021-05-30 23:55:08 -0300

    Update useForm.jsx

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-//extraemos la logica de las forms, que de otra forma se repetiría a lo largo de las secciones de una página
+
 export const useForm = ( initialState = {} ) => {
 
     const [values, setValues] = useState(initialState);
