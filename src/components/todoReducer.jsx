685655ccad2480ef93aa292580fe1fd7a9f8feff
Colorinche <68034479+AndreaLecuona@users.noreply.github.com>
2021-05-30 23:54:27 -0300

    Update todoReducer.jsx

--- src/components/todoReducer.jsx
+++ src/components/todoReducer.jsx
@@ -2,33 +2,20 @@
 export const todoReducer = (state = [], action) => {
     
     switch ( action.type ) {
-        //debe conocer de antemano todas las posibles 'acciones' que le mandemos
+
         case 'add':
             return [...state, action.payload];
 
         case 'delete':
-            return state.filter( todo => todo.id !== action.payload); //action-payload toma el id
+            return state.filter( todo => todo.id !== action.payload);
 
         case 'toggle':
             return state.map( todo => 
                 ( todo.id === action.payload ) ? { ...todo, done: !todo.done } : todo
             );
 
-        //VERSION LARGA DE TOGGLE
-        // case 'toggle':
-        //     return state.map( todo => {
-        //         if( todo.id === action.payload ){
-        //             return {
-        //                 ...todo,
-        //                 done: !todo.done
-        //             }
-        //         } else {
-        //             return todo;
-        //         }
-        //     })
-
         default:
-            return state; //para asegurarnos que devuelva un state
+            return state; 
     }
 
 };
